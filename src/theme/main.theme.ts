6f9be10de8f856e5295c3fbd4d4fa69a00b9a5f1
Erick Giraldo <erickgiraldo94@gmail.com>
2024-12-06 11:31:12 -0500

    feat: changed app's colors

--- src/theme/main.theme.ts
+++ src/theme/main.theme.ts
@@ -1,27 +1,28 @@
 export const theme = {
   colors: {
-    background: '#121212',
-    textPrimary: '#FFFFFF',
-    textSecondary: '#B0B0B0',
-    accent: '#4c87e6',
-    backgroundLight: '#2C2C2C',
-    borderColor: '#3A3A3A',
-    error: '#de0a26',
-    success: '#3b8132',
-    transparent: '#00000080',
+    background: "#344E41",
+    backgroundLighter: "#3A5A40",
+    borderColor: "#588157",
+    accent: "#A3B18A",
+    backgroundLight: "#DAD7CD",
+    textPrimary: "#FFFFFF",
+    textSecondary: "#B0B0B0",
+    error: "#de0a26",
+    success: "#3b8132",
+    transparent: "#00000080",
   },
   fonts: {
     default: {
-      fontFamily: 'Sans-Serif', // Change this to your desired font family (e.g., 'Roboto', 'Arial')
-      fontWeight: '400' as 'normal' | '400',
+      fontFamily: "Sans-Serif", // Change this to your desired font family (e.g., 'Roboto', 'Arial')
+      fontWeight: "400" as "normal" | "400",
     },
     bold: {
-      fontFamily: 'Sans-Serif',
-      fontWeight: 'bold',
+      fontFamily: "Sans-Serif",
+      fontWeight: "bold",
     },
     italic: {
-      fontFamily: 'Sans-Serif',
-      fontStyle: 'italic',
+      fontFamily: "Sans-Serif",
+      fontStyle: "italic",
     },
   },
   fontSizes: {
